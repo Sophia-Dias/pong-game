05bc72b85a6cc27f9453466c3589af70eecb92b5
Sophia-Dias <76136379+Sophia-Dias@users.noreply.github.com>
2021-04-20 19:18:38 -0300

    Cria as variáveis principais

---
+++ script.js
@@ -0,0 +1,40 @@
+// Elementos
+var BtIniciar;
+var Bola;
+var Cpu;
+var jogador;
+var txtPontos;
+
+// Controle de Animação
+var game, frames;
+
+// Posições
+var posBolaX, posBolaY;
+var posJogadorX, posJogadorY;
+var posCpuX, posCpuY;
+
+// Direção de acordo com a tecla
+var dirJogY
+
+// Posições Iniciais
+var posJogIniY = 180;
+var posCpuIniY = 180;
+var posBolaIniY = 240, posBolaIniX = 475;
+
+// Tamanhos
+var campoX = 0, campoY = 0, campoW = 960, campoH = 500;
+var barraW = 20, barraH = 140;
+var bolaW = 20, bolaH = 20;
+
+// Direção
+var bolaX, bolaY;
+var cpuY = 0;
+
+// Velocidade
+var velBola;
+var velCpu;
+var velJog;
+
+// Controle
+var pontos = 0;
+var tecla;
